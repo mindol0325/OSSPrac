e7e63ca37a957600b71d9fe2e9a28e53ffca71dc
mindol0325 <2020110491@dgu.ac.kr>
2024-10-02 22:02:46 +0900

    1002 add school

--- Prac5/main.py
+++ Prac5/main.py
@@ -1,8 +1,12 @@
 name = input('이름을 입력하세요 : ')
 num = input('학번을 입력하세요 :')
+major = input('학과를 입력하세요 : ')
+school = input('학교을 입력하세요 : ')
 
 
 
 print('<출력>')
 print(f'이름:{name}')
 print(f'학번:{num}')
+print(f'학과:{major}')
+print(f'학년:{school}')
